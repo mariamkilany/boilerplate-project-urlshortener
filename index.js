606e121d23057ead79062ec634420bd865eb58c8
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2022-10-31 00:59:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+var bodyParser = require('body-parser')
+const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,9 +19,29 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let map1 = new Map()
+let map2 = new Map()
+let index= 1;
+app.post('/api/shorturl',urlencodedParser,function(req, res) {
+  var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+  if(!map1.has(req.body.url)&&req.body.url.match(regex)){
+        map1.set(req.body.url,index);
+        map2.set(index,req.body.url)
+        index++;
+  }
+if(!req.body.url.match(regex)){
+  res.json({error: 'invalid url'})
+}
+  res.json({original_url : req.body.url ,         
+  short_url:map1.get(req.body.url)})
+  
 });
+
+app.get('/api/shorturl/:short',(req,res)=>{
+  // console.log(map2.get(parseInt(req.params.short)))
+  res.redirect(map2.get(parseInt(req.params.short)))
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
